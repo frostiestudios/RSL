f9500afacf714d31b2ccc8a49af49c54776fea74
thememetsar <mattwiner04@gmail.com>
2023-01-07 15:17:47 -0800

    Client and Server now commited

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 import socket
+import webbrowser
 from appJar import gui
+
 def receive_message():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("0.0.0.0", 12345))
@@ -10,21 +12,29 @@ def receive_message():
     # Receive and decode the message
     data = conn.recv(1024).decode()
     print(f"Received: {data}")
-    app.setLabel("Message", data)
+    # Call the appropriate function based on the message received
+    if data == "Open Link 1":
+        open_link_1()
+    elif data == "Open Link 2":
+        open_link_2()
     # Close the socket
     conn.close()
     receive_message()
 
+def open_link_1():
+    webbrowser.open("https://www.example.com/link1")
 
-app=gui("Listener")
+def open_link_2():
+    webbrowser.open("https://www.example.com/link2")
+
+app = gui("Listener")
 print("App Running")
 print("No Server Running Yet")
+
 app.addLabel("Server Manager")
-app.addLabel("Message","---")
+app.addLabel("Message", "---")
+
 app.startFrame("IP INFO")
-app.addLabel("Your IP","Your IP: 192.168.0.121",1,2)
-app.addLabel("Host IP","Host IP: 192.168.0.79",2,2)
-app.addIcon("Connection",iconName="Wi-Fi",row=1,column=1,rowspan=2)
-app.stopFrame()
-app.setLabelFont(20)
-app.go()
+app.addLabel("Your IP", "Your IP: 192.168.0.121", 1, 2)
+app.addLabel("Host IP", "Host IP: 192.168.0.79", 2, 2)
+app.addIcon
--- client.py
+++ client.py
@@ -1,6 +1,31 @@
+import socket
 from appJar import gui
 
-app=gui("Client")
-app.addLabel("Client Manager")
-app.setLabelFont(20)
+def send_message(button):
+    # Create a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to the server
+    s.connect(("192.168.0.121", 12345))
+    # Send a message to the server
+    s.sendall(button.encode())
+    # Close the socket
+    s.close()
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+app = gui("Hub")
+ip_address = get_ip_address()
+print("App Running")
+print("HUB RUNNING AT IP:",ip_address)
+
+app.addButtons(["Open Link 1", "Open Link 2"], [send_message, send_message])
+
+app.startFrame("IP INFO")
+app.addLabel("Your IP", f"Your IP: {ip_address}", 1, 2)
+app.addIcon("Connection", iconName="Wi-Fi", row=1, column=1)
+app.stopFrame()
+
 app.go()
