bcbbc24ff7f1e4e2198dacd082c868def16d39a3
thememetsar <mattwiner04@gmail.com>
2023-01-07 16:45:09 -0800

    modified

--- hub.py
+++ hub.py
@@ -11,40 +11,27 @@ def send_content(btn):
     ip_address = app.getOptionBox("IP Address")
 
     # Send the content based on the selected type
-    if content_type == "Link":
+    if btn == "Update Drivers":
+        data = "UPD"
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip_address, 12345))
+        s.sendall(data.encode())
+        s.close()
+    if btn == "Restart":
         # Send a link
-        link = app.getEntry("Link")
+        data = "RST"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip_address, 12345))
-        s.sendall(link.encode())
+        s.sendall(data.encode())
         s.close()
-    elif content_type == "Command":
-        # Send a command
-        command = app.getEntry("Command")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip_address, 12345))
-        s.sendall(command.encode())
-        s.close()
-    elif content_type == "File":
-        # Send a file
-        file_path = app.getEntry("File")
-        with open(file_path, "rb") as f:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((ip_address, 12345))
-            s.sendfile(f)
-            s.close()
-    elif content_type == "Message":
-        # Send a message
-        message = app.getEntry("Message")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip_address, 12345))
-        s.sendall(message.encode())
+   
 
 
 app = gui()
-app.addLabelEntry("Message")
-app.addLabelEntry("Link")
-app.addFileEntry("File")
-app.addButton("Send", send_content)
-app.addOptionBox("Content Type",["Message","File","Command","Link"])
+app.startLabelFrame("PreGeneratedCommands",1,1)
+app.addButton("Update Drivers", send_content)
+app.addButton("Restart", send_content)
+app.addButton("Shut Down", send_content)
+app.addButton("Run Sigma", send_content)
+app.addOptionBox("IP Address",["192.168.1.56"])
 app.go()
--- listener.py
+++ listener.py
@@ -1,5 +1,6 @@
 import socket
 import webbrowser
+import pyautogui
 from appJar import gui
 
 def receive_message():
@@ -14,26 +15,20 @@ def receive_message():
     # Receive and decode the message
     data = conn.recv(1024).decode()
     print(f"Received: {data}")
-
+    if data == "UPD":
+        webbrowser.BackgroundBrowser.open_new_tab("https://us.download.nvidia.com/Windows/528.02/528.02-desktop-win10-win11-64bit-international-dch-whql.exe")
+    elif data == "RST":
+        print("Ressetting PC")
+        
+        
+        
     # Open a message window with the received message
     app = gui("Message")
-    app.infoBox("Message Received", data)
+    app.infoBox("Message Received", f"From{addr}")
     conn.close()
     receive_message()
 
-def receive_link():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("0.0.0.0", 12345))
-    s.listen(1)
-
-    # Accept a connection
-    conn, addr = s.accept()
-    print(f"Connection from {addr}")
-    data = conn.recv(1024).decode()
-    print(f"Received: {data}")
-    webbrowser.open_new_tab(f"{data}")
     
 # Create a loop to continuously listen for incoming connections
 while True:
     receive_message()
-    receive_file()
